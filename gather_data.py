35ff4e5c92a30d186ea99cb07a100ab128ff0745
Mason Acevedo <mason.acevedo2000@gmail.com>
2025-07-30 00:33:58 -0700

    remove inference

--- gather_data.py
+++ gather_data.py
@@ -1,7 +1,7 @@
 import torchvision.datasets
 import torchvision.transforms as transforms
 
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, random_split, DataLoader
 from torchvision import models
 
 
@@ -10,20 +10,3 @@ folder = "cifar_10_imagery"
 dataset = torchvision.datasets.CIFAR10(root=folder, download=True)
 
 model = models.resnet18(weights=models.ResNet18_Weights.DEFAULT)
-
-model.eval()
-
-PIL_image = dataset[0][0]
-
-transform = transforms.Compose([
-    transforms.Resize(256),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[.485, .456, .406], std=[.229, .224, .225])
-])
-
-image_as_tensor = transform(PIL_image).unsqueeze(0)
-
-outputs = model(image_as_tensor)
-
-print(type(outputs))
-print(outputs.shape)
