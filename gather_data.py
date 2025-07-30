da40fe8c15395e351000ab962da2d5331aa86079
Mason Acevedo <mason.acevedo2000@gmail.com>
2025-07-30 00:34:42 -0700

    add in dataloader

--- gather_data.py
+++ gather_data.py
@@ -11,3 +11,4 @@ dataset = torchvision.datasets.CIFAR10(root=folder, download=True)
 
 model = models.resnet18(weights=models.ResNet18_Weights.DEFAULT)
 training_data, val_data = random_split(dataset, [0.8, 0.2])
+dataloader = DataLoader(training_data, batch_size=32, shuffle=True, num_workers=4)
