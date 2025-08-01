c6f6b697aeefc2160166d1b17fcf925637824cb1
Mason Acevedo <mason.acevedo2000@gmail.com>
2025-08-01 00:22:37 -0700

    change model to output 10 categories instead of 1000

--- gather_data.py
+++ gather_data.py
@@ -11,6 +11,8 @@ folder = "cifar_10_imagery"
 dataset = torchvision.datasets.CIFAR10(root=folder, download=True)
 
 model = models.resnet18(weights=models.ResNet18_Weights.DEFAULT)
+model.fc = nn.linear(model.fc.in_features, 10)
+
 training_data, val_data = random_split(dataset, [0.8, 0.2])
 dataloader = DataLoader(training_data, batch_size=32, shuffle=True, num_workers=4)
 
