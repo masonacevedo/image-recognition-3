accc2789ababb2c907aa381a23a37012d626b8ce
Mason Acevedo <mason.acevedo2000@gmail.com>
2025-08-03 19:47:08 -0700

    add in all transforms

--- gather_data.py
+++ gather_data.py
@@ -16,12 +16,13 @@ model.fc = nn.linear(model.fc.in_features, 10)
 training_data, val_data = random_split(dataset, [0.8, 0.2])
 dataloader = DataLoader(training_data, batch_size=32, shuffle=True, num_workers=4)
 
-transformations = [
+transformations = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),           # Add padding and random crop
+    transforms.RandomHorizontalFlip(p=0.5),         # Random horizontal flip
+    transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
     transforms.ToTensor(),
-    transforms.RandomHorizontalFlip(p=0.5),
-    transforms.RandomRotation(degrees=15),
-    transforms.RandomAffine(degrees=0, translate=(0.1, 0.1))
-]
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+])
 
 transform = transforms.Compose(transformations)
 image, label = dataset[0]
