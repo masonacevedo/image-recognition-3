a6c561e9204336e51481ce3ab47a4cdfc5ceec61
Mason Acevedo <mason.acevedo2000@gmail.com>
2025-08-03 19:55:28 -0700

    remove normalization - image un-recognizable after transformation

--- gather_data.py
+++ gather_data.py
@@ -21,7 +21,6 @@ transform = transforms.Compose([
     transforms.RandomHorizontalFlip(p=0.5),         # Random horizontal flip
     transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
     transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
 
 image, label = dataset[0]
