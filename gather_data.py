1c57dbeaca4b8e2cb7256c994df0528b65ee9ee4
Mason Acevedo <mason.acevedo2000@gmail.com>
2025-08-03 19:51:52 -0700

    add more transforms

--- gather_data.py
+++ gather_data.py
@@ -11,12 +11,12 @@ folder = "cifar_10_imagery"
 dataset = torchvision.datasets.CIFAR10(root=folder, download=True)
 
 model = models.resnet18(weights=models.ResNet18_Weights.DEFAULT)
-model.fc = nn.linear(model.fc.in_features, 10)
+# model.fc = nn.linear(model.fc.in_features, 10)
 
 training_data, val_data = random_split(dataset, [0.8, 0.2])
 dataloader = DataLoader(training_data, batch_size=32, shuffle=True, num_workers=4)
 
-transformations = transforms.Compose([
+transform = transforms.Compose([
     transforms.RandomCrop(32, padding=4),           # Add padding and random crop
     transforms.RandomHorizontalFlip(p=0.5),         # Random horizontal flip
     transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
@@ -24,7 +24,6 @@ transformations = transforms.Compose([
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
 
-transform = transforms.Compose(transformations)
 image, label = dataset[0]
 image.show()
 
