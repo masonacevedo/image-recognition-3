448bcd6ef2a8286759e32673060a8ddbadbc6b54
Mason Acevedo <mason.acevedo2000@gmail.com>
2025-08-12 16:57:15 -0700

    perfoming inference successfully in training mode

--- train_model.py
+++ train_model.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 import torchvision.datasets
 import torchvision.transforms.functional as F
-
+import torch.nn as nn
 
 def show_tensor_as_image(t, batch_dimension_index=0):
     img = t[batch_dimension_index].detach().cpu().numpy()
@@ -15,25 +15,27 @@ def show_tensor_as_image(t, batch_dimension_index=0):
     plt.show()
 
 
-resnet18 = models.resnet18(weights=models.ResNet18_Weights.DEFAULT)
-resnet18.eval()
+myModel = models.resnet18(weights=models.ResNet18_Weights.DEFAULT)
 
 folder = "cifar_10_imagery"
 dataset = torchvision.datasets.CIFAR10(root=folder, download=True)
+print("type(dataset):", type(dataset))
+print("len(dataset):", len(dataset))
+print("type(dataset[0]):", type(dataset[0]))
+print("type(dataset[0][0]):", type(dataset[0][0]))
+print("type(dataset[0][1]):", type(dataset[0][1]))
 
-for i in range(0, 5):
-    image, label = dataset[i]
-    random_image = torch.randn(1, 3, 200, 200)
-    image_as_tensor = F.to_tensor(image).reshape(1,3,32,32)
+loss_function = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(myModel.parameters(), lr=0.001)
 
-    result_logits = resnet18(image_as_tensor)
+BATCH_SIZE = 8
+EPOCHS = 10
 
-    result_probs = torch.softmax(result_logits, dim=1)
-    top_5_probs, top_5_categories = torch.topk(result_probs, 5)
+myModel.train()
+for epoch in range(0, EPOCHS):
 
-
-    category_names = models.ResNet18_Weights.DEFAULT.meta["categories"]
-    for index in list(top_5_categories[0]):
-        print(category_names[index])
-    show_tensor_as_image(image_as_tensor)
-    input()
+    print("Epoch:", epoch)
+    
+    x = torch.stack([F.to_tensor(dataset[j][0]) for j in range(0, BATCH_SIZE)])
+    y = myModel(x)
+    
