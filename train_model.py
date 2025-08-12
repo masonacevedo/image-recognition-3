6d187a5f35f2e0629a35f4073695664bcbfb8d25
Mason Acevedo <mason.acevedo2000@gmail.com>
2025-08-12 00:44:58 -0700

    create a random image and perform inference on it using resnet18!

--- train_model.py
+++ train_model.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 resnet18 = models.resnet18(weights=models.ResNet18_Weights.DEFAULT)
+category_names = models.ResNet18_Weights.DEFAULT.meta["categories"]
 
 resnet18.eval()
 
@@ -16,4 +17,15 @@ def show_tensor_as_image(t, batch_dimension_index=0):
     plt.imshow(img)
     plt.show()
 
-show_tensor_as_image(random_image)
+# show_tensor_as_image(random_image)
+
+result_logits = resnet18(random_image)
+
+result_probs = torch.softmax(result_logits, dim=1)
+top_5_probs, top_5_categories = torch.topk(result_probs, 5)
+
+print(top_5_probs)
+print(top_5_categories)
+
+for index in list(top_5_categories[0]):
+    print(category_names[index])
