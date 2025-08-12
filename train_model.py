8c336013c3a236f0cfe46c1bc7347e6048e15e93
Mason Acevedo <mason.acevedo2000@gmail.com>
2025-08-12 15:54:24 -0700

    rearrange for better readability

--- train_model.py
+++ train_model.py
@@ -3,12 +3,6 @@ import torchvision.models as models
 import numpy as np
 import matplotlib.pyplot as plt
 
-resnet18 = models.resnet18(weights=models.ResNet18_Weights.DEFAULT)
-category_names = models.ResNet18_Weights.DEFAULT.meta["categories"]
-
-resnet18.eval()
-
-random_image = torch.randn(1, 3, 200, 200)
 
 def show_tensor_as_image(t, batch_dimension_index=0):
     img = t[batch_dimension_index].detach().cpu().numpy()
@@ -17,15 +11,15 @@ def show_tensor_as_image(t, batch_dimension_index=0):
     plt.imshow(img)
     plt.show()
 
-# show_tensor_as_image(random_image)
 
+resnet18 = models.resnet18(weights=models.ResNet18_Weights.DEFAULT)
+resnet18.eval()
+random_image = torch.randn(1, 3, 200, 200)
 result_logits = resnet18(random_image)
 
 result_probs = torch.softmax(result_logits, dim=1)
 top_5_probs, top_5_categories = torch.topk(result_probs, 5)
 
-print(top_5_probs)
-print(top_5_categories)
-
+category_names = models.ResNet18_Weights.DEFAULT.meta["categories"]
 for index in list(top_5_categories[0]):
     print(category_names[index])
