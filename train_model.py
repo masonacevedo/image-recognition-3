b9d0e8581ff94c9971a35b341656864b3fa2774f
Mason Acevedo <mason.acevedo2000@gmail.com>
2025-08-12 17:44:41 -0700

    training model successfully!

--- train_model.py
+++ train_model.py
@@ -28,16 +28,32 @@ print("type(dataset[0][1]):", type(dataset[0][1]))
 loss_function = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(myModel.parameters(), lr=0.001)
 
-BATCH_SIZE = 8
-EPOCHS = 10
+BATCH_SIZE = 16
+EPOCHS = 25
 
 myModel.train()
+x_axis = []
+y_axis = []
 for epoch in range(0, EPOCHS):
-
     print("Epoch:", epoch)
     # images = [dataset[epoch*BATCH_SIZE+j][0] for j in range(0, BATCH_SIZE)]
     # for image in images:
     #     show_tensor_as_image(F.to_tensor(image).reshape(1,3,32,32))
     x = torch.stack([F.to_tensor(dataset[epoch*BATCH_SIZE+j][0]) for j in range(0, BATCH_SIZE)])
-    y = myModel(x)
+    labels = torch.tensor([dataset[epoch*BATCH_SIZE+j][1] for j in range(0, BATCH_SIZE)])
+    predictions = myModel(x)
+    loss = loss_function(predictions, labels)
+    print("loss:", loss)
+    loss.backward()
+    optimizer.step()
+    optimizer.zero_grad()
+
+
+    x_axis.append(epoch)
+    y_axis.append(loss.item())
+
+plt.plot(x_axis, y_axis)
+plt.title("Epochs vs Loss")
+plt.show()
+
     
