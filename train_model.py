947d5de095254e21c1afbcb8212d5993525d19ac
Mason Acevedo <mason.acevedo2000@gmail.com>
2025-08-15 10:59:09 -0700

    add transformations - model isn't training any better :/

--- train_model.py
+++ train_model.py
@@ -7,6 +7,7 @@ import torchvision.datasets
 
 import torch.nn.functional as F
 import torchvision.transforms.functional as TF
+import torchvision.transforms as transforms
 
 import torch.nn as nn
 
@@ -21,12 +22,6 @@ def show_tensor_as_image(t, batch_dimension_index=0):
     plt.imshow(img)
     plt.show()
 
-def custom_collate_fn(batch):
-    images, labels = zip(*batch)
-    images = torch.stack([TF.to_tensor(image) for image in images])
-    labels = torch.tensor(labels)
-    return images, labels
-
 myModel = models.resnet18(weights=models.ResNet18_Weights.DEFAULT)
 myModel.fc = nn.Linear(myModel.fc.in_features, 10)
 myModel.to(device)
@@ -38,11 +33,44 @@ myModel.to(device)
 #     param.requires_grad = True
 
 folder = "cifar_10_imagery"
-dataset = torchvision.datasets.CIFAR10(root=folder, download=True)
-training_data, val_data, _ = random_split(dataset, [0.80, 0.20, 0.0])
+raw_dataset = torchvision.datasets.CIFAR10(root=folder, download=True)
+raw_training_data, val_data, _ = random_split(raw_dataset, [0.80, 0.20, 0.0])
 
-training_loader = DataLoader(training_data, batch_size=16, shuffle=True, num_workers=0, collate_fn=custom_collate_fn)
-validation_loader = DataLoader(val_data, batch_size=16, shuffle=True, num_workers=0, collate_fn=custom_collate_fn)
+# TODO: ADD NORMALIZATION!
+training_transform = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(p=0.5),
+    transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4, hue=0.2),
+    transforms.ToTensor(),
+])
+
+validation_transform = transforms.ToTensor()
+
+class TransformedDataset(torch.utils.data.Dataset):
+    def __init__(self, dataset, transform=None):
+        self.dataset = dataset
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, index):
+        image, label = self.dataset[index]
+        return self.transform(image), label
+
+
+
+transformed_training_data = TransformedDataset(raw_training_data, training_transform)
+transformed_validation_data = TransformedDataset(val_data, validation_transform)
+
+print("Type of transformed_training_data[0]:", type(transformed_training_data[0]))
+print("Type of transformed_training_data[0][0]:", type(transformed_training_data[0][0]))
+print("Type of transformed_training_data[0][1]:", type(transformed_training_data[0][1]))
+
+training_loader = DataLoader(transformed_training_data, batch_size=16, shuffle=True, num_workers=0)
+validation_loader = DataLoader(transformed_training_data, batch_size=16, shuffle=True, num_workers=0)
+
+
 
 loss_function = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(myModel.parameters(), lr=0.001)
