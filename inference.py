6a244309a90f4ea610ac79327e5772258bc3c400
Mason Acevedo <mason.acevedo2000@gmail.com>
2025-08-16 15:13:12 -0700

    make inference script more robust

--- inference.py
+++ inference.py
@@ -28,44 +28,40 @@ transforms = transforms.Compose([
     # transforms.Normalize(mean=[0.4914, 0.4822, 0.4465], std=[0.2470, 0.2435, 0.2616]),
 ])
 
-base_model = "models/90_percent_no_augmentation_with_normalization"
-models = [base_model + f"_epoch_{i}.pth" for i in range(0,30)]
+model_base_name = input("model base name:")
+all_models = os.listdir("models")
 
+raw_model_paths = list(filter(lambda s: model_base_name in s, all_models))
+model_paths = [os.path.join("models", name) for name in raw_model_paths]
 
-for index, model in enumerate(models):
+# sort according to epoch number at the end
+model_paths.sort(key=lambda x: int(x.split("_epoch_")[-1].split(".pth")[0]))
+
+for model_index, model in enumerate(model_paths):
     correct_count = 0
-    for image in images:
-        image_as_tensor = transforms(image).unsqueeze(0).to(device)
+    images_as_tensors = [transforms(image).to(device) for image in images]
+    images_as_tensors = torch.stack(images_as_tensors)
 
-        myModel = torch.load(model, weights_only=False)
-        myModel.eval()
+    myModel = torch.load(model, weights_only=False)
+    myModel.eval()
 
-        myModel.to(device)
+    myModel.to(device)
 
-        with torch.no_grad():
-            logits = myModel(image_as_tensor)
-            probabilities = F.softmax(logits, dim=1)
-            
-            predicted_index = torch.argmax(logits, dim=1).item()
-            predicted_category = CIFAR10_CLASSES[predicted_index]
-            if predicted_category == "bird":
-                correct_count += 1
-            
-            # print(f"Raw logits: {logits}")
-            # print(f"Probabilities: {probabilities}")
-            # print(f"Predicted index: {predicted_index}")
-            # print(f"Predicted category: {predicted_category}")
-            # print(f"P(bird):            {probabilities[0][2]}")
-            # print()
-            
-            # print("\nAll probabilities:")
-            # for i, (class_name, prob) in enumerate(zip(CIFAR10_CLASSES, probabilities[0])):
-            #     print(f"{i}: {class_name}: {prob:.4f}")
+    with torch.no_grad():
+        logits = myModel(images_as_tensors)
+        probabilities = F.softmax(logits, dim=1)
+        # print("probabilities: ", probabilities)
 
-    # print(f"Correct count: {correct_count}")
-    # print(f"Total count: {len(images)}")
-    # print(f"Accuracy: {correct_count / len(images)}")
-    print(f"{index}:{correct_count}/{len(images)}")
+        predicted_indices = torch.argmax(logits, dim=1)
+        # print("predicted_indices: ", predicted_indices)
+        # input("Press Enter to continue...")
+
+    for predicted_index in predicted_indices:
+        predicted_category = CIFAR10_CLASSES[predicted_index]
+        if predicted_category == "bird":
+            correct_count += 1
+
+    print(f"{model}:{correct_count}/{len(images)}")
     print()
 
 
